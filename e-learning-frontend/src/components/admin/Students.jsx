0823a5faef803192083d15d186b1c07f51dfbfee
Moustafa <moustafaism013@hotmail.com>
2022-10-08 18:40:43 +0300

    Frontnd: Added the students cards in the admin

--- e-learning-frontend/src/components/admin/Students.jsx
+++ e-learning-frontend/src/components/admin/Students.jsx
@@ -1,7 +1,23 @@
+import StudentCard from "./StudentCard"
 
 function Students() {
     return (
-        <div>Students</div>
+        <div className="page">
+            <div className="page-title">
+                <p className="large-text bold-text">Students</p>
+            </div>
+            <div className="students-cards-wrapper">
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+                <StudentCard />
+            </div>
+        </div>
     )
 }
 
