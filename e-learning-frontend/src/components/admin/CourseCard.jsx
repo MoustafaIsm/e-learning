ddc075375b7d18ab1e83bf9834bf4d14c82ad38c
Moustafa <moustafaism013@hotmail.com>
2022-10-08 20:27:26 +0300

    Frontend: Added the course cards for the course tab

---
+++ e-learning-frontend/src/components/admin/CourseCard.jsx
@@ -0,0 +1,15 @@
+
+function CourseCard() {
+    return (
+        <div className="course-card">
+            <div className="course-info">
+                <p>Course code</p>
+                <p>Course name</p>
+                <p>credits</p>
+                <p>Assigned to :</p>
+            </div>
+        </div>
+    )
+}
+
+export default CourseCard
--- e-learning-frontend/src/components/admin/Courses.jsx
+++ e-learning-frontend/src/components/admin/Courses.jsx
@@ -1,7 +1,20 @@
+import CourseCard from "./CourseCard";
 
 function Courses() {
     return (
-        <div>Courses</div>
+        <div className="page">
+            <div className="page-title">
+                <p className="large-text bold-text">Courses</p>
+            </div>
+            <div className="courses-cards-wrapper">
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+                <CourseCard />
+            </div>
+        </div>
     )
 }
 
