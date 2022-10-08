ed46179043ce0e10f09d2d59bf6db51fcc3b3856
Moustafa <moustafaism013@hotmail.com>
2022-10-08 22:55:41 +0300

    Backend: Created migration forannouncements collection

---
+++ e-learning-server/database/migrations/2022_10_08_195418_create_announcements_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function ($collection) {
+            $collection->id();
+            $collection->integer('course_id');
+            $collection->integer('instructor_id');
+            $collection->string('announcement');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+};
