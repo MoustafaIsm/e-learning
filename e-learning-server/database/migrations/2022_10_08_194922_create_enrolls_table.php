176547601d73fbdb3eb90038c2e3c3a5093a9f87
Moustafa <moustafaism013@hotmail.com>
2022-10-08 22:51:27 +0300

    Backend: Created migration for enrolls collection

---
+++ e-learning-server/database/migrations/2022_10_08_194922_create_enrolls_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('enrolls', function ($collection) {
+            $collection->integer('course_id');
+            $collection->integer('student_id');
+            $collection->timestamps();
+
+            $collection->primary(['course_id', 'student_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('enrolls');
+    }
+};
