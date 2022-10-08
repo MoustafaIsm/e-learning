6b24c482998cbf522004137494345365bb4ebc91
Moustafa <moustafaism013@hotmail.com>
2022-10-08 22:57:45 +0300

    Backend: Created migration for assignments submits collection

---
+++ e-learning-server/database/migrations/2022_10_08_195605_create_assignment_submits_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('assignment_submits', function ($collection) {
+            $collection->id();
+            $collection->integer('assignmnet_id');
+            $collection->integer('student_id');
+            $collection->string('file_url');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('assignment_submits');
+    }
+};
