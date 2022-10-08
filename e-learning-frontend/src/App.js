ed0764d21f6fffaa31d4fc4513f7af32627e21b7
Moustafa <moustafaism013@hotmail.com>
2022-10-08 08:19:27 +0300

    Frontend: Added my main components

--- e-learning-frontend/src/App.js
+++ e-learning-frontend/src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+import Admin from './components/Admin';
+import Instructor from './components/Instructor';
+import Student from './components/Student';
 
 function App() {
-  return (
-    <div>
+	const [token, setToken] = useState("");
 
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path='/login' element={<Login />} />
+				<Route path='/admin' element={<Admin />} />
+				<Route path='/Instructor' element={<Instructor />} />
+				<Route path='/Student' element={<Student />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
