06e2888b3c0158c409591742e290896f83228a12
Moustafa <moustafaism013@hotmail.com>
2022-10-09 18:25:02 +0300

    Linking: Fixed the login hook

--- e-learning-frontend/src/hooks/login.js
+++ e-learning-frontend/src/hooks/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-export const deleteStudent = async (url, formData) => {
+export const loginUser = async (url, formData) => {
     const response = await axios.post(`${url}/auth/login`, formData);
-    const data = await response.json();
+    const data = await response.data;
     return data;
 }
