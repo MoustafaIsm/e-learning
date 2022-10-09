82c949c4e03334d66ddccdf3409910a794a4d52f
Moustafa <moustafaism013@hotmail.com>
2022-10-09 08:16:29 +0300

    Backend: Added database seeders

--- e-learning-server/config/app.php
+++ e-learning-server/config/app.php
@@ -183,6 +183,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         Jenssegers\Mongodb\MongodbServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
 
         /*
          * Package Service Providers...
