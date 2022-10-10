a4babd8d0542ec3d79a8c8674b7ccced14d25cc2
Moustafa <moustafaism013@hotmail.com>
2022-10-10 16:03:12 +0300

    Linking: linked the add course hook to the add course button

--- e-learning-frontend/src/components/admin/Courses.jsx
+++ e-learning-frontend/src/components/admin/Courses.jsx
@@ -3,10 +3,16 @@ import AddCouseModal from './AddCourseModal';
 import { fetchCourses } from '../../hooks/admin/fetchCourses';
 import { useState } from "react";
 import { useEffect } from "react";
+import { addCourse } from '../../hooks/admin/addCourse';
 
 function Courses({ url, token }) {
 
     const [courses, setCourses] = useState([]);
+
+    const addNewCourse = async (url, token, course) => {
+        const data = await addCourse(url, token, course);
+        setCourses([...courses, data.course]);
+    }
 
     useEffect(() => {
         const getCourses = async () => {
@@ -20,7 +26,11 @@ function Courses({ url, token }) {
         <div className="page">
             <div className="page-title">
                 <p className="large-text bold-text">Courses</p>
-                <AddCouseModal />
+                <AddCouseModal
+                    url={url}
+                    token={token}
+                    onAdd={addNewCourse}
+                />
             </div>
             <div className="courses-cards-wrapper">
                 {
